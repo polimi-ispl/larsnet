41eb4c63adf466597d68e9c20b10180192914ba3
Alessandro Ilic Mezza <alessandroilic.mezza@polimi.it>
2023-12-18 08:01:32 +0100

    Update paths/Add device

--- separate.py
+++ separate.py
@@ -5,7 +5,7 @@ import soundfile as sf
 import argparse
 
 
-def separate(input_dir: Union[str, Path], output_dir: Union[str, Path], wiener_exponent: Optional[float]):
+def separate(input_dir: Union[str, Path], output_dir: Union[str, Path], wiener_exponent: Optional[float], device: str):
     input_dir = Path(input_dir)
     output_dir = Path(output_dir)
 
@@ -18,7 +18,7 @@ def separate(input_dir: Union[str, Path], output_dir: Union[str, Path], wiener_e
     larsnet = LarsNet(
         wiener_filter=wiener_exponent is not None,
         wiener_exponent=wiener_exponent,
-        device='cpu',
+        device=device,
         config="config.yaml",
     )
 
@@ -34,13 +34,16 @@ def separate(input_dir: Union[str, Path], output_dir: Union[str, Path], wiener_e
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_dir', type=str, required=True, help='Path to the root directory where to find the target drum mixtures.')
-    parser.add_argument('-o', '--output_dir', type=str, default='separated_stems', help='Path to the directory where to save the separated tracks.')
-    parser.add_argument('-w', '--wiener_exponent', type=float, default=None, help='Positive α-Wiener filter exponent (float). Use it only if Wiener filtering is to be applied.')
+    parser.add_argument('-i', '--input_dir', type=str, required=True, help="Path to the root directory where to find the target drum mixtures.")
+    parser.add_argument('-o', '--output_dir', type=str, default='separated_stems', help="Path to the directory where to save the separated tracks.")
+    parser.add_argument('-w', '--wiener_exponent', type=float, default=None, help="Positive α-Wiener filter exponent (float). Use it only if Wiener filtering is to be applied.")
+    parser.add_argument('-d', '--device', type=str, default='cpu', help="Torch device. Default 'cpu'")
+
     args = vars(parser.parse_args())
 
     separate(
         input_dir=args['input_dir'],
         output_dir=args['output_dir'],
-        wiener_exponent=args['wiener_exponent']
+        wiener_exponent=args['wiener_exponent'],
+        device=args['device']
     )
